f771771cb67075a3d3605f50cd037d8cab0704a6
holly <hongl@kaolafm.com>
2016-05-05 18:40:38 +0800

    添加获取当前JS的绝对路径方法

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 #js常用方法整理
 ###参考司徒正美的《JavaScript框架设计》
 ######快速查找需要的方法，可查看：http://jslib.hongliang.org/
+
 ######添加18位身份证的验证方法idcard.js
+
+######添加获取当前JS的绝对路径getCurrAbsPath.js
---
+++ getCurrAbsPath.js
@@ -0,0 +1,42 @@
+/*
+ * 获取当前JS的绝对路径
+ */
+;(function(exports){
+	var doc = exports.document,
+		a = {},
+		expose = +new Date(),
+		rExtractUri = /((?:http|https|file):\/\/.*?\/[^:]+)(?::\d+)?:\d+/,
+		isLtIE8 = ('' + doc.querySelector).indexOf('[native code]') === -1;
+	exports.getCurrAbsPath = function(){
+		// FF,Chrome
+		if (doc.currentScript){
+			return doc.currentScript.src;
+		}
+
+		var stack;
+		try{
+			a.b();
+		}
+		catch(e){
+			stack = e.fileName || e.sourceURL || e.stack || e.stacktrace;
+		}
+		// IE10
+		if (stack){
+			var absPath = rExtractUri.exec(stack)[1];
+			if (absPath){
+				return absPath;
+			}
+		}
+
+		// IE5-9
+		for(var scripts = doc.scripts,
+			i = scripts.length - 1,
+			script; script = scripts[i--];){
+			if (script.className !== expose && script.readyState === 'interactive'){
+				script.className = expose;
+				// if less than ie 8, must get abs path by getAttribute(src, 4)
+				return isLtIE8 ? script.getAttribute('src', 4) : script.src;
+			}
+		}
+	};
+}(window));
